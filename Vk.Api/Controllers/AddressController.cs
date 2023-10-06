4d2445e101230dff32b38da24e4da1656779e77f
[Zorbey] <[zorbeyycelikk@outlook.com]>
2023-10-06 13:25:30 +0300

    AddressController.cs eklendi

---
+++ Vk.Api/Controllers/AddressController.cs
@@ -0,0 +1,61 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Vk.Base.Response;
+using Vk.Operation.Cqrs;
+using Vk.Schema;
+
+namespace VkApi.Controllers;
+
+[Route("vk/api/v1/[controller]")]
+[ApiController]
+
+public class AddressController : ControllerBase
+{
+    private IMediator mediator;
+
+    public AddressController(IMediator mediator) 
+    {
+        this.mediator = mediator;
+    }
+
+
+    [HttpGet]
+    public async Task<ApiResponse<List<AddressResponse>>> GetAll()
+    {
+        var operation = new GetAllAddressQuery();
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ApiResponse<AddressResponse>> Get(int id)
+    {
+        var operation = new GetAddressByIdQuery(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpPost]
+    public async Task<ApiResponse<AddressResponse>> Post([FromBody] AddressRequest request)
+    {
+        var operation = new CreateAddressCommand(request);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ApiResponse> Put(int id, [FromBody] AddressRequest request)
+    {
+        var operation = new UpdateAddressCommand(request, id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ApiResponse> Delete(int id)
+    {
+        var operation = new DeleteAddressCommand(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+}
